87c7713897ffbc8e607d1497ba38855bec4660fe
Ben Wainwright <87009396+benwainwrightcinch@users.noreply.github.com>
2021-08-03 07:54:36 +0100

    Initial commit

---
+++ src/roman.spec.ts
@@ -0,0 +1,7 @@
+test("One should equal one", () => {
+  expect(1).toEqual(1);
+});
+
+test("One should equal zero", () => {
+  expect(1).toEqual(0);
+});
---
+++ src/roman.ts
@@ -0,0 +1,18 @@
+/*
+ * Write a function to convert decimal numbers
+ * to a roman numeral string.
+ *
+ * e.g.
+ *  1 -> "I"
+ *  2 -> "II"
+ *  4 -> "IV"
+ *  23 -> "XXIII"
+ *  231 -> "CCXXXI"
+ *  1000 -> "M"
+ */
+export const toRoman = (decimal: number) => "";
+
+/*
+ * If you finish the above task early,
+ * try doing it in reverse */
+export const fromRoman = (characters: string) => 0;
